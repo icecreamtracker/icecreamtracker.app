963677ecfbb82d5d949474181a1cfa7e82e7d316
Justin Heintz <justinpaulheintz@gmail.com>
2015-05-15 17:44:36 +0000

    think im good

---
+++ www/js/index.js
@@ -0,0 +1,39 @@
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+$( document ).bind( "mobileinit", function() { $.mobile.allowCrossDomainPages = true; });
+$(function(){
+	$.mobile.allowCrossDomainPages = true;
+	//setInterval(function(){myloc()}, 10000);
+});
+function myloc(){
+	navigator.geolocation.getCurrentPosition(
+	function(position) {
+		$.get('https://bluejaydev.com/dev/icrecream/?lat='+position.coords.latitude+"&lng="+position.coords.longitude, function (data) {
+			$(".data").html(data);
+		});
+	},
+	function() {
+		alert('Error getting location');
+	});
+}
